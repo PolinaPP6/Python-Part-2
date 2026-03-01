9f2d6c0f273de400088ba8a58d3a6598e6881e0f
PolinaPP6 <106628916+PolinaPP6@users.noreply.github.com>
2026-03-01 22:16:22 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,62 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+
+    def name(self):
+        return self.name
+
+
+    def author(self):
+        return self.author
+
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook:
+    def __init__(self, name: str, author: str, pages: int):
+        self.name = name
+        self.author = author
+        self.pages = pages
+
+
+    def pages(self):
+        return self.pages
+
+
+    def pages(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Количество страниц должно быть целым числом")
+        if value <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self.pages = value
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+
+class AudioBook:
+    def __init__(self, name: str, author: str, duration: float):
+        self.name = name
+        self.author = author
+        self.duration = duration
+
+    def duration(self):
+        return self.duration
+
+    def duration(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("Продолжительность должна быть числом")
+        if value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self.duration = float(value)
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
